cd945311def342150f3fde422ca6d1f1f5f572da
Connor <cfr3.14156@gmail.com>
2021-11-10 10:44:50 -0700

    Stretch 2

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -15,8 +15,12 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate
     @IBOutlet weak var choicesStackView: UIStackView!
     @IBOutlet weak var computerDisplay: UIImageView!
     @IBOutlet weak var resultText: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     var myChoice = -1
     var computerChoice = -1
+    var timeElapsed:Float = 0.0
+    var timeAllowed:Float = 3.0
+    var timer:Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,6 +35,8 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate
             }
         }
         if(self.myChoice != -1){
+            timer?.invalidate()
+            timer = nil
             var name = "hjkl;'"
             if(computerChoice == 0){name = "rock"}
             if(computerChoice == 1){name = "paper"}
@@ -57,6 +63,25 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate
         let svc = SFSafariViewController(url: url!)
         svc.delegate = self
         self.present(svc, animated: true, completion: nil)
+    }
+    @IBAction func start(_ sender: Any) {
+        timeElapsed = 0.0
+        self.resultText.text = "Result"
+        self.computerDisplay.image = nil
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
+    }
+    @objc func countDown(){
+        timeElapsed += 0.1
+        self.timerLabel.text = "\(round((timeAllowed - timeElapsed) * 10) / 10)"
+        if timeElapsed >= timeAllowed - 0.1{
+            if myChoice == -1{
+                //lose
+                self.resultText.text = "Timed Out :("
+                timer?.invalidate()
+                timer = nil
+            }
+        }
     }
     
 
